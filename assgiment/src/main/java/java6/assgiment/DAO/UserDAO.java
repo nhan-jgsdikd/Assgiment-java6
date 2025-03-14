686362fe7ac7d3225577b5c32aefed26b9766ce0
nhan-jgsdikd <animehistwo@gmail.com>
2025-03-14 11:58:45 +0700

    upload lan 1

---
+++ assgiment/src/main/java/java6/assgiment/DAO/UserDAO.java
@@ -0,0 +1,9 @@
+package java6.assgiment.DAO;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java6.assgiment.Entity.User;
+
+public interface UserDAO extends JpaRepository<User, Long> {
+
+}
---
+++ assgiment/src/test/java/java6/assgiment/AssgimentApplicationTests.java
@@ -0,0 +1,13 @@
+package java6.assgiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssgimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
