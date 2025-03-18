d11a40f5171a3bce7cbac81cb3026decbb5ea9e7
Cuonglech <cuonglcpd10358@gmail.com>
2025-03-18 14:38:45 +0700

    upload CSDL lần 2

---
+++ CSDL/Banner.sql
@@ -0,0 +1,6 @@
+SELECT * FROM asmjava6.banner;
+INSERT INTO `asmjava6`.`banner` (`id`, `name`, `photo`) VALUES ('1', 'banner1', 'img/banner1.jpg');
+INSERT INTO `asmjava6`.`banner` (`id`, `name`, `photo`) VALUES ('2', 'banner2', 'img/banner2jpg.jpg');
+INSERT INTO `asmjava6`.`banner` (`id`, `name`, `photo`) VALUES ('3', 'banner3', 'img/banner3.png');
+INSERT INTO `asmjava6`.`banner` (`id`, `name`, `photo`) VALUES ('4', 'banner4', 'img/banner4.png');
+INSERT INTO `asmjava6`.`banner` (`id`, `name`, `photo`) VALUES ('5', 'banner5', 'img/banner5.jpg');
