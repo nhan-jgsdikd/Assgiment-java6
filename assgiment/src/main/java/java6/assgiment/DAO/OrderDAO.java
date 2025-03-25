c63e0d60dce6cc61471b28a87a9a9f6f76f39382
nhan-jgsdikd <animehistwo@gmail.com>
2025-03-26 01:26:34 +0700

    Delete assgiment directory

--- assgiment/src/main/java/java6/assgiment/DAO/OrderDAO.java
+++
@@ -1,8 +0,0 @@
-package java6.assgiment.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java6.assgiment.Entity.Orders;
-
-public interface OrderDAO extends JpaRepository<Orders, Long> {
-}
